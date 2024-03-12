75c0b643d2b186f667cb706f88533e37893da4f5
7vTr0x7 <7vtrox7@gmail.com>
2024-03-13 01:36:52 +0530

    Ts Function with Object

--- build/index.js
+++ build/index.js
@@ -1,21 +1,11 @@
 "use strict";
-const person1 = {
-    height: 172,
-    weight: 300,
-    adult: true,
+const getData = (product) => {
+    console.log(product.name);
 };
-const person2 = {
-    height: 170,
-    weight: 280,
+const productOne = {
+    name: "apple",
+    stock: 1,
+    price: 399,
+    url: "apple.jpg",
 };
-const newPerson = {
-    height: 180,
-    weight: 290,
-    graduate: false,
-};
-const newPersonObj = {
-    add: (n, m) => {
-        console.log(n + m);
-    },
-};
-newPersonObj.add(10, 20);
+getData(productOne);
