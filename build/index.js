52dde5636cf55914152e903c7b534c014b62d3a3
7vTr0x7 <7vtrox7@gmail.com>
2024-03-13 03:20:46 +0530

    Ts Type assertion

--- build/index.js
+++ build/index.js
@@ -1,12 +1,22 @@
 "use strict";
-class newProduct {
-    constructor(name, stock, price) {
-        this.name = name;
-        this.stock = stock;
-        this.price = price;
-        this.id = String(Math.random() * 1000);
-        this.getId = () => this.id;
-    }
-}
-const ipad = new newProduct("ipad", 80, 100);
-ipad.getId;
+// Type Assertion
+//const btn = document.getElementById("btn") as HTMLElement;
+//OR
+//const btn = document.getElementById("btn")!;
+//OR
+// const btn = <HTMLElement>document.getElementById("btn");
+// btn.onclick;
+// const img = document.getElementById("img") as HTMLImageElement;
+// img.src;
+const form = document.getElementById("form");
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+btn.onclick = form.submit;
+form.onsubmit = (e) => {
+    e.preventDefault();
+    const h2 = document.createElement("h2");
+    const value = Number(input.value);
+    const body = document.querySelector("body");
+    h2.textContent = String(value * 20);
+    body.append(h2);
+};
