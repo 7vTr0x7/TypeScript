e13dc9970c9d03c59b61a335406c7b00ab71f22d
7vTr0x7 <7vtrox7@gmail.com>
2024-03-13 02:42:40 +0530

    Ts Classes with interface

--- build/index.js
+++ build/index.js
@@ -1,45 +1,12 @@
 "use strict";
-// Access Modifiers
-// - public : can be used any where
-// - private : only be used inside its own class
-// - protected : only be used inside its derived class
-class Player {
-    constructor(
-    // access modifiers
-    height, weight, power) {
-        this.height = height;
-        this.weight = weight;
-        this.power = power;
-        this.id = "126v";
-    }
-    // getHeight = () => this.height;
-    // OR
-    // Getter function
-    get getHeight() {
-        return this.height;
-    }
-    // setHeight = (val: number) => {
-    //   this.height = val;
-    // };
-    // OR
-    // Setter Function
-    set setHeight(val) {
-        // required one parameter only
-        this.height = val;
+class newProduct {
+    constructor(name, stock, price) {
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+        this.id = String(Math.random() * 1000);
+        this.getId = () => this.id;
     }
 }
-const abhi = new Player(172, 39, 40);
-console.log("height: ", abhi.getHeight);
-abhi.setHeight = 180;
-console.log("height: ", abhi.getHeight);
-class Player2 extends Player {
-    constructor(height, weight, power, special) {
-        super(height, weight, power);
-        this.special = special;
-    }
-    get getPower() {
-        return this.power;
-    }
-}
-const ex = new Player2(173, 50, 60, true);
-console.log("Power: ", ex.getPower);
+const ipad = new newProduct("ipad", 80, 100);
+ipad.getId;
