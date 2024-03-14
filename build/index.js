2de0b014a9cab94408c0084e462927cb13bdde46
7vTr0x7 <7vtrox7@gmail.com>
2024-03-14 13:32:17 +0530

    Ts type Utility - Record<keys,type>

--- build/index.js
+++ build/index.js
@@ -1,10 +1,7 @@
 "use strict";
-const user = {
-    name: "user",
-    email: "email.com",
-};
-// OR
-const User2 = {
-    name: "user",
-    email: "email.com",
+const exUser1 = {
+    vtrox: { age: 20 },
+    sam: { age: 20 },
+    omen: { age: 20 },
+    yoru: { age: 20 },
 };
