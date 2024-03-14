26a6b6ad7d889f3b78df334b12e83dfb473ac870
7vTr0x7 <7vtrox7@gmail.com>
2024-03-14 13:08:02 +0530

    Ts type Utility - Partial<type>

--- build/index.js
+++ build/index.js
@@ -1,18 +1,1 @@
 "use strict";
-const newPerson = {
-    name: "sam",
-    email: "email@gmail.com",
-};
-// keyof type assertion
-let key = "name";
-//newPerson[key as keyof Person];
-// if we don't have access to interface
-newPerson[key];
-// in function
-const getData = (key) => {
-    return newPerson[key];
-};
-// OR
-const getData1 = (key) => {
-    return newPerson[key];
-};
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,6 @@
-interface Person {
-  // [key: string]: string; // dynamic key - // index signature
+type User = {
   name: string;
   email: string;
-}
-
-const newPerson: Person = {
-  name: "sam",
-  email: "email@gmail.com",
 };
 
-// keyof type assertion
-
-let key = "name";
-
-//newPerson[key as keyof Person];
-
-// if we don't have access to interface
-
-newPerson[key as keyof typeof newPerson];
-
-// in function
-
-const getData = (key: keyof Person): string => {
-  return newPerson[key];
-};
-
-// OR
-
-const getData1 = (key: keyof typeof newPerson): string => {
-  return newPerson[key];
-};
+type User2 = Partial<User>;
