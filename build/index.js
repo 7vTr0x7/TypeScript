9aff4498910a6ddf764d0379793bcb5e6aea5f65
7vTr0x7 <7vtrox7@gmail.com>
2024-03-14 14:40:54 +0530

    Ts Generics

--- build/index.js
+++ build/index.js
@@ -1,11 +1,12 @@
 "use strict";
-class Ex {
-    constructor(a, b) {
-        this.a = a;
-        this.b = b;
-    }
-}
-const obj = {
-    a: 1,
-    b: 2,
+// Generics
+const func = (val) => {
+    let newVal;
+    return val;
 };
+// T works as an place holder which will be replaced by the given argument
+// It can be any Alphabet ex. T , A or CustomType
+// mostly used is T
+const ans = func(20);
+const ans2 = func("20");
+const ans3 = func(true);
