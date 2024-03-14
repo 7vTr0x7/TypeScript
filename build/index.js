49e082337c51c93d0869b1f775b1ef39d6d73875
7vTr0x7 <7vtrox7@gmail.com>
2024-03-14 12:47:01 +0530

    Ts keyof

--- build/index.js
+++ build/index.js
@@ -1,22 +1,18 @@
 "use strict";
-// Type Assertion
-//const btn = document.getElementById("btn") as HTMLElement;
-//OR
-//const btn = document.getElementById("btn")!;
-//OR
-// const btn = <HTMLElement>document.getElementById("btn");
-// btn.onclick;
-// const img = document.getElementById("img") as HTMLImageElement;
-// img.src;
-const form = document.getElementById("form");
-const btn = document.getElementById("btn");
-const input = document.getElementById("input");
-btn.onclick = form.submit;
-form.onsubmit = (e) => {
-    e.preventDefault();
-    const h2 = document.createElement("h2");
-    const value = Number(input.value);
-    const body = document.querySelector("body");
-    h2.textContent = String(value * 20);
-    body.append(h2);
+const newPerson = {
+    name: "sam",
+    email: "email@gmail.com",
+};
+// keyof type assertion
+let key = "name";
+//newPerson[key as keyof Person];
+// if we don't have access to interface
+newPerson[key];
+// in function
+const getData = (key) => {
+    return newPerson[key];
+};
+// OR
+const getData1 = (key) => {
+    return newPerson[key];
 };
