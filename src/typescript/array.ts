7c817a434ab05fca66f926a149d272af890f3f99
7vTr0x7 <7vtrox7@gmail.com>
2024-03-17 19:44:15 +0530

    Ts array

---
+++ src/typescript/array.ts
@@ -0,0 +1,13 @@
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+console.log(numbers);
+
+const numbers2: Array<number> = [12, 3, 4, 5, 6, 7];
+const str1: Array<string> = ["!", "s", "g"];
+
+const arr: Array<string | number | boolean> = ["s", 1, true];
+
+console.log(arr);
+
+// tuple / fixed size array
+const ex1: [number, string, boolean] = [1, "s", true];
