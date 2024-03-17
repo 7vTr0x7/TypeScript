a53f451544de597f54530d4e3ed18791848cc291
7vTr0x7 <7vtrox7@gmail.com>
2024-03-17 19:50:05 +0530

    Ts function with object

---
+++ src/typescript/functionWithObj.ts
@@ -0,0 +1,23 @@
+interface Product {
+  name: string;
+  stock: number;
+  price: number;
+  url: string;
+  readonly id: string;
+}
+
+type GetDataType = (product: Product) => void;
+
+const getData: GetDataType = (product): void => {
+  console.log(product.name);
+};
+
+const productOne: Product = {
+  name: "apple",
+  stock: 1,
+  price: 399,
+  url: "apple.jpg",
+  id: "235",
+};
+
+getData(productOne);
