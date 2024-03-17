4959936023fea74bc0f972b9d79e4be38866cfa2
7vTr0x7 <7vtrox7@gmail.com>
2024-03-17 19:52:38 +0530

    Ts never type

---
+++ src/typescript/neverType.ts
@@ -0,0 +1,13 @@
+const err = new Error(); // type Error
+
+const errorHandler = (): Error => {
+  // returns error
+  return new Error();
+};
+
+// throwing error returns type never
+
+const errorHandler2 = (): never => {
+  // returns error
+  throw new Error();
+};
